459b66b75fee36f283adf249433a171e71fb5240
lzy49@qq.com <lzy49@qq.com>
2022-04-20 13:25:35 +0800

    style: 增加注释

--- src/runtime-core/componentSlot.ts
+++ src/runtime-core/componentSlot.ts
@@ -1,8 +1,9 @@
 import { ShapeFlags } from "../shared/ShapeFlags";
 
+// slot 必须是一个 对象
+// initSlots 的作用是将 vnode.children 中非函数转换为函数方便 renderSlot调用。
 export const initSlots = (instance, children) => {
-  console.log(children);
-  
+
   // 如果是个对象
   const { vnode } = instance;
   if (vnode.shapeFlag & ShapeFlags.SLOTS_CHILDREN) {
--- src/runtime-core/helpers/renderSlot.ts
+++ src/runtime-core/helpers/renderSlot.ts
@@ -1,4 +1,5 @@
 import { createVnode } from '../createVnode'
+// renderSlot 的功能就是将 传递过来的 key => function 执行掉。
 export const renderSlot = (slots, key, props) => {
   if (slots[key]) {
     return createVnode('div', {}, slots[key](props));
